75e9750b1094522ac122f3671a292e1eac28f8b4
Metthew <joematt0706@gmail.com>
2020-08-04 01:27:09 +0900

    componentDidMount呼び出し
    renderの後、componentDidMountを呼び出して、stateを設定する

--- src/App.js
+++ src/App.js
@@ -4,22 +4,18 @@ import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count : 0
+    isLoading : true
   };
-  add = () => {
-    this.setState(current => ({ count : current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count : current.count - 1 }));
-  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState( {isLoading : false} );
+    }, 6000);
+  }
+
   render() {
-    return (
-      <div>
-        <h1>The Number : {this.state.count}</h1>
-        <button onClick = {this.add}>Add</button>
-        <button onClick = {this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading....." : "We are ready"}</div>;
   }
 }
 
