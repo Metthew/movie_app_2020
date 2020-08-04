9a6d7b5205f6e13c4cd4fa995f8e37d565f1870c
Metthew <joematt0706@gmail.com>
2020-08-04 23:20:01 +0900

    axiosインストール
    axiosインストール
    Life Cycle method利用
    ES6のasync, await利用してaxiosする

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from "axios";
 import PropTypes from "prop-types";
 
 
@@ -7,10 +8,12 @@ class App extends React.Component {
     isLoading : true
   };
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState( {isLoading : false} );
-    }, 6000);
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  };
+
+   componentDidMount() {
+     this.getMovies();
   }
 
   render() {
